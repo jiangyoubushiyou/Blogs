55827e1d3b75924b2ec14ba5b522fa0c6a4e31e9
“jiangyoubushiyou <563108916@qq.com>
2017-04-06 22:23:44 +0800

    test

---
+++ config/default.js
@@ -0,0 +1,9 @@
+module.export = {
+    port: 3000,
+    session: {
+        secret: 'Blog',
+        key: 'Blog',
+        maxAge:2592000000
+    },
+    mongodb:'mongodb://localhost:27017/Blog'
+};
